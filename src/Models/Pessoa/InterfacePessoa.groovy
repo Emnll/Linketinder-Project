58f6706cd91b4ff9623811cd9562f4c62cc66178
Emnll <emanuellegmnsa@gmail.com>
2025-02-21 15:54:27 -0300

    Implementacao da Interface Pessoa

--- src/Models/Pessoa/InterfacePessoa.groovy
+++ src/Models/Pessoa/InterfacePessoa.groovy
@@ -1,7 +1,20 @@
 package Models.Pessoa
-/*A interface pessoa ira conter ira conter os metodos obrigatorios para os perfis de candidato e empresa*/
+/*A interface pessoa ira conter ira conter os metodos obrigatorios para os perfis de candidato e empresa
+* Ou seja, todos os getters e setters dos atributos de pessoa*/
 
 interface InterfacePessoa {
+
+    String getNome()
+    void setNome(String nome)
+    String getEmail()
+    void setEmail(String email)
+    String getEstado()
+    void setEstado(String estado)
+    String getCEP()
+    void setCEP(String CEP)
+    String getDescricao()
+    void setDescricao(String descricao)
+
 
 
 }
